31b467d4db818a69eee4238258db94ea5cd06c3c
iftachfridental <ifridental58@gmail.com>
2018-01-19 16:37:31 +0200

    add News

--- News.cpp
+++ News.cpp
@@ -4,11 +4,38 @@
 
 #include "News.h"
 
+using namespace std;
+
 class News{
 
-private:
-    int type;
-    int prod_id;
-    int news_id;
+public:
+    News::News(NewsType type, int prod_id, int prod_type_id) {
+        this->prod_id = prod_id;
+        this->type = type;
+        this->prod_type_id = prod_type_id;
+    }
+
+    friend ostream &operator<<(ostream &outs, const News *obj) {
+
+        outs << obj->toString();
+        return outs;
+    }
+
+    string News::toString() {
+        string type_name = "";
+        switch (type) {
+            case sports:
+                type_name = "SPORTS";
+                break;
+            case weather:
+                type_name = "WEATHER";
+                break;
+            case regularNews:
+                type_name = "NEWS";
+                break;
+        }
+        string s = "Producer " + prod_id + " " + type_name + " " + prod_type_id;
+        return s;
+    }
 
 };
--- News.h
+++ News.h
@@ -5,4 +5,27 @@
 #ifndef OS4_NEWS_H
 #define OS4_NEWS_H
 
+#include <iostream>
+#include <string>
+
+enum NewsType {
+    regularNews, weather, sports
+};
+
+class News {
+private:
+    NewsType type;
+    int prod_id;
+    int prod_type_id;
+
+public:
+    News(NewsType type, int prod_id, int prod_type_id);
+
+    friend ostream &operator<<(ostream &outs, const News *obj);
+
+    std::string toString();
+};
+
+
+
 #endif //OS4_NEWS_H
